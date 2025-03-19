c2a8e9379c71674f33487ff41e49c9700fde6658
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-19 23:24:17 +0000

    correção

--- models/2.silver/fat_category.sql
+++ models/2.silver/fat_category.sql
@@ -1,6 +1,6 @@
 SELECT
-    p.product_category_name as category,
+    p.product_category_name AS category,
     oi.price as preco,
     oi.freight_value as frete
-FROM {{ ref('products') }} as p
-INNER JOIN {{ ref('order_items') }} as oi USING(product_id)
+FROM {{ ref('products') }} AS
+INNER JOIN {{ ref('order_items') }}AS oi USING(product_id)
