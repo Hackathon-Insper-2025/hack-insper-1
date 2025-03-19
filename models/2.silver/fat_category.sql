2f182036e824a0c00e4389d0385d6ebe1818567b
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-19 23:04:29 +0000

    correção de erro

--- models/2.silver/fat_category.sql
+++ models/2.silver/fat_category.sql
@@ -1,7 +1,7 @@
-select 
+SELECT
     p.product_category_name as category,
     ROUND(SUM(oi.price + oi.freight_value), 2) AS total_price
-from {{ ref('products') }} as p
+FROM {{ ref('products') }} as p
 INNER JOIN {{ ref('order_items') }} as oi USING(product_id)
 GROUP BY product_category_name
 HAVING p.product_category_name IS NOT NULL
