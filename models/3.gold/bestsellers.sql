9903c450700936b75881be4e61878faa12f80360
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-20 15:32:27 +0000

    correção bestseller

--- models/3.gold/bestsellers.sql
+++ models/3.gold/bestsellers.sql
@@ -4,7 +4,6 @@ SELECT
 FROM {{ ref('fat_category') }} 
 WHERE order_delivered_customer_date IS NOT NULL  
 GROUP BY product_category_name
-
 HAVING category IS NOT NULL AND TRIM(category) <> ''
 ORDER BY total_sold DESC
 
