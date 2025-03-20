48a167291a02cf040ae5f47733f1fc2559e7fd58
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-20 15:37:30 +0000

    correção

--- models/3.gold/bestsellers.sql
+++ models/3.gold/bestsellers.sql
@@ -2,7 +2,7 @@ SELECT
     product_category_name AS category, 
     COUNT(order_id) AS total_sold
 FROM {{ ref('fat_category') }} 
-WHERE order_delivered_customer_date IS NOT NULL and product_category_name IS NOT NULL AND TRIM(product_category_name) <> '' 
+WHERE product_category_name IS NOT NULL AND TRIM(product_category_name) <> '' 
 GROUP BY product_category_name
 ORDER BY total_sold DESC
 
