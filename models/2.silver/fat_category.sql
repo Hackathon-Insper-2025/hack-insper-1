c9b36354bf98fee2cbcfb8706558a38c15931517
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-20 00:04:03 +0000

    correção de camada
    não está indo pro metabase ainda...

--- models/2.silver/fat_category.sql
+++ models/2.silver/fat_category.sql
@@ -1,8 +1,7 @@
 SELECT
-    p.product_category_name as category,
-    oi.price as preco,
-    oi.freight_value as frete
+    p.product_category_name,
+    oi.price,
+    oi.freight_value
 FROM {{ ref('products') }} AS p
     INNER JOIN {{ ref('order_items') }} AS oi USING(product_id)
-WHERE p.product_category_name IS NOT NULL
-    AND TRIM(p.product_category_name) <> ''
+
--- models/3.gold/categoryprice.sql
+++ models/3.gold/categoryprice.sql
@@ -1,6 +1,7 @@
 SELECT 
-    category,
-    SUM(preco + frete) AS total_price
+    product_category_name as category,
+    SUM(price + freight_value) AS total_price
 FROM {{ ref('fat_category') }} as fc
 GROUP BY category
+HAVING category IS NOT NULL AND TRIM(category) <> ''
 ORDER BY total_price DESC
