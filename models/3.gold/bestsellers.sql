950f5d75b76a8e7ef32a2a75a51bedcd541de8ae
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-20 15:36:37 +0000

    commit para visualização no metabase

--- models/3.gold/bestsellers.sql
+++ models/3.gold/bestsellers.sql
@@ -2,9 +2,8 @@ SELECT
     product_category_name AS category, 
     COUNT(order_id) AS total_sold
 FROM {{ ref('fat_category') }} 
-WHERE order_delivered_customer_date IS NOT NULL  
+WHERE order_delivered_customer_date IS NOT NULL and product_category_name IS NOT NULL AND TRIM(product_category_name) <> '' 
 GROUP BY product_category_name
-HAVING category IS NOT NULL AND TRIM(category) <> ''
 ORDER BY total_sold DESC
 
 
--- models/3.gold/entregas_reviews.sql
+++ models/3.gold/entregas_reviews.sql
@@ -1,15 +1,11 @@
 SELECT
     review_score as nota_review,
-    
     CASE 
         WHEN timestamp(NULLIF(order_delivered_customer_date, '')) > timestamp(NULLIF(order_estimated_delivery_date, '')) THEN 'Atrasado'
         ELSE 'No prazo'
     END AS status_entrega,
-    
     COUNT(*) AS total_reviews
-    
-FROM {{ ref('fat_category') }} 
-    
+FROM {{ ref('fat_category') }}
 GROUP BY review_score, status_entrega
 ORDER BY review_score DESC, status_entrega
 
