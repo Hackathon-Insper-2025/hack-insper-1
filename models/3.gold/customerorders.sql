dc1fd98d0311fcbde81546d2e41fab1abbd919c1
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-22 21:21:00 +0000

    customerorders

--- models/3.gold/customerorders.sql
+++ models/3.gold/customerorders.sql
@@ -2,7 +2,7 @@ SELECT
     customer_unique_id as cliente,
     COUNT(order_id) as pedidos_clientes,
     ROUND(SUM(price),2) as total_gasto_cliente,
-    ROUND(SUM(price) / COUNT(order_id), 2) AS ticket_medio
+    ROUND(SUM(price) / (COUNT(order_id)), 2) AS ticket_medio
 FROM {{ ref('fat_category') }} as fc
 GROUP BY customer_unique_id
 HAVING customer_unique_id IS NOT NULL
