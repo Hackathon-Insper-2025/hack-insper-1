2cf8994c0b826a5c7df989516123024c8c00852d
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-22 19:10:30 +0000

    Adicionando perfil clientes

--- models/3.gold/customerorders.sql
+++ models/3.gold/customerorders.sql
@@ -1,8 +1,8 @@
 SELECT 
     customer_id as cliente,
-    COUNT(order_id) as pedidos_clientes
+    COUNT(order_id) as pedidos_clientes,
+    ROUND(SUM(price),2) as total_gasto_cliente
 FROM {{ ref('fat_category') }} as fc
 GROUP BY customer_id
-WHERE review_score = 1
-HAVING customer_id IS NOT NULL AND TRIM(customer_id) <> ''
+HAVING customer_id IS NOT NULL
 ORDER BY pedidos_clientes DESC
