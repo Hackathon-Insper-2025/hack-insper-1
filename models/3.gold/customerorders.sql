d1a6324781a3040a890c3b5b69aa68452e38193a
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-22 22:13:59 +0000

    Adicionando ticket medio

--- models/3.gold/customerorders.sql
+++ models/3.gold/customerorders.sql
@@ -1,10 +1,17 @@
-SELECT 
-    customer_unique_id as cliente,
-    COUNT(order_id) as pedidos_clientes,
-    ROUND(SUM(price),2) as total_gasto_cliente,
-    ROUND(SUM(price) / (COUNT(order_id)), 2) AS ticket_medio
-    
-FROM {{ ref('fat_category') }} as fc
-WHERE customer_unique_id IS NOT NULL
-GROUP BY customer_unique_id
-ORDER BY pedidos_clientes DESC
+SELECT
+  *,
+  CASE 
+    WHEN ticket_medio < 100 THEN 'Baixo (<100)'
+    WHEN ticket_medio BETWEEN 100 AND 300 THEN 'Médio (100-300)'
+    ELSE 'Alto (>300)'
+  END AS faixa_ticket
+FROM (
+  SELECT
+    customer_unique_id AS cliente,
+    COUNT(order_id) AS pedidos_clientes,
+    ROUND(SUM(price), 2) AS total_gasto_cliente,
+    ROUND(SUM(price) / COUNT(order_id), 2) AS ticket_medio
+  FROM {{ ref('fat_category') }}
+  WHERE customer_unique_id IS NOT NULL
+  GROUP BY customer_unique_id
+) AS sub
