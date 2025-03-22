a3f4fce055b64041d2e061658d0876b2c6efb3b2
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-22 19:46:04 +0000

    Corrigindo entregas_review

--- models/3.gold/entregas_reviews.sql
+++ models/3.gold/entregas_reviews.sql
@@ -1,10 +1,10 @@
 SELECT
     review_score as nota_review,
+    COUNT(*) AS qtde_reviews,
     CASE 
         WHEN timestamp(NULLIF(order_delivered_customer_date, '')) > timestamp(NULLIF(order_estimated_delivery_date, '')) THEN 'Atrasado'
         ELSE 'No prazo'
-    END AS status_entrega,
-    COUNT(*) AS qtde_reviews
+    END AS status_entrega
 FROM {{ ref('fat_category') }}
 GROUP BY review_score, status_entrega
 ORDER BY review_score DESC, status_entrega
