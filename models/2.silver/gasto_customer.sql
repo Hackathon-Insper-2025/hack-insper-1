18a3807141822d4b3f28283cce5e8770b77da3af
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-23 18:04:04 +0000

    gasto por estado

--- models/2.silver/gasto_customer.sql
+++ models/2.silver/gasto_customer.sql
@@ -6,4 +6,5 @@ SELECT
 FROM {{ ref('customers') }} AS c
 JOIN {{ ref('orders') }} AS o USING (customer_id)
 JOIN {{ ref('order_items') }} AS oi USING (order_id)
+JOIN {{ ref('customers') }} AS c ON g.geolocation_state = c.customer_state
 GROUP BY c.customer_unique_id, c.customer_id, c.customer_state
