01ccd9d0115616ed1193610aa1353137d2dd14a2
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-23 18:53:27 +0000

    .

--- models/3.gold/palavrasimpact.sql
+++ models/3.gold/palavrasimpact.sql
@@ -1,19 +1,19 @@
 WITH palavras_tokenizadas AS (
-  SELECT 
+    SELECT 
     LOWER(TRIM(word)) AS palavra,
     review_score
-  FROM {{ ref('review') }},
-  UNNEST(SPLIT(review_comment_message, ' ')) AS word
-  WHERE review_comment_message IS NOT NULL
+    FROM {{ ref('review') }},
+    UNNEST(SPLIT(review_comment_message, ' ')) AS word
+    WHERE review_comment_message IS NOT NULL
 )
 
 SELECT 
-  palavra,
-  COUNT(*) AS ocorrencias,
-  ROUND(AVG(review_score), 2) AS media_nota
+    palavra,
+    COUNT(*) AS ocorrencias,
+    ROUND(AVG(review_score), 2) AS media_nota
 FROM palavras_tokenizadas
 WHERE palavra NOT IN ('o', 'a', 'e', 'de', 'do', 'da', 'que', 'em', 'é', '.', ',', '-', '', 'para', 'por', 'com', 'na', 'no', 'os', 'as')
-  AND LENGTH(palavra) > 2
+    AND LENGTH(palavra) > 2
 GROUP BY palavra
 ORDER BY ocorrencias DESC
 LIMIT 50
