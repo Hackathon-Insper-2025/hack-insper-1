800ddbdfb43b3963adef205349d4a9eda0bd3af7
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-23 19:55:50 +0000

    arrumando lat e long

--- models/3.gold/gasto_estado.sql
+++ models/3.gold/gasto_estado.sql
@@ -1,7 +1,7 @@
 SELECT 
   estado,
   ROUND(SUM(total_gasto), 2) AS total_gasto_estado,
-  ROUND(AVG(latitude), 4) AS latitude,
-  ROUND(AVG(longitude), 4) AS longitude
+  ROUND(AVG(latitude), 4) AS valor_lt,
+  ROUND(AVG(longitude), 4) AS valor_lg
 FROM {{ ref('gasto_customer') }}
 GROUP BY estado
