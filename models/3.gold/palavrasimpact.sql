0c4c1a1f505e8bc1594b31409bf084604d42d38a
Julia Diniz <julia.diniz@insperdata1.com>
2025-03-25 00:25:51 +0000

    correção palavras impact

--- models/3.gold/palavrasimpact.sql
+++ models/3.gold/palavrasimpact.sql
@@ -1,7 +1,7 @@
 WITH palavras_tokenizadas AS (
     SELECT 
-    LOWER(TRIM(word)) AS palavra,
-    review_score
+        LOWER(TRIM(word)) AS palavra,
+        review_score
     FROM {{ ref('review') }},
     UNNEST(SPLIT(review_comment_message, ' ')) AS word
     WHERE review_comment_message IS NOT NULL
@@ -12,17 +12,25 @@ SELECT
     COUNT(*) AS ocorrencias,
     ROUND(AVG(review_score), 2) AS media_nota
 FROM palavras_tokenizadas
-WHERE palavra NOT IN 
-    WHERE palavra NOT IN (
-        'o', 'a', 'e', 'de', 'do', 'da', 'que', 'em', 'é', '.', ',', '-', '', 
-        'para', 'por', 'com', 'na', 'no', 'os', 'as', 'um', 'uma', 'uns', 'umas', 
-        'dos', 'das', 'sobre', 'entre', 'desde', 'contra', 'perante', 'mas', 'ou', 
-        'se', 'então', 'como', 'porque', 'quando', 'pois', 'ele', 'ela', 'eles', 
-        'elas', 'isso', 'isto', 'aquilo', 'meu', 'minha', 'seu', 'sua', 'dele', 
-        'dela', 'nosso', 'nossa', 'ser', 'estar', 'ter', 'fazer', 'haver', 'vai', 
-        'vou', 'foi', 'são', 'era', 'muito', 'pouco', 'tudo', 'nada', 'todos', 
-        'todas', 'mesmo', 'assim', 'aqui', 'ali', 'lá'
-    ) 
-    AND LENGTH(palavra) > 2
+WHERE palavra NOT IN (
+    'o', 'a', 'e', 'de', 'do', 'da', 'que', 'em', 'é', '.', ',', '-', '',
+    'para', 'por', 'com', 'na', 'no', 'nos', 'nas', 'os', 'as',
+    'um', 'uma', 'uns', 'umas', 'ao', 'aos', 'à', 'às',
+    'dos', 'das', 'sobre', 'entre', 'desde', 'contra', 'perante',
+    'mas', 'ou', 'se', 'então', 'como', 'porque', 'quando', 'pois',
+    'ele', 'ela', 'eles', 'elas', 'isso', 'isto', 'aquilo',
+    'meu', 'minha', 'meus', 'minhas', 'seu', 'sua', 'seus', 'suas',
+    'dele', 'dela', 'deles', 'delas', 'nosso', 'nossa', 'nossos', 'nossas',
+    'ser', 'estar', 'ter', 'fazer', 'haver', 'vai', 'vou', 'foi', 'são', 'era', 'sou', 'está', 'estavam',
+    'muito', 'pouco', 'tudo', 'nada', 'todos', 'todas', 'algum', 'alguma', 'alguns', 'algumas',
+    'mesmo', 'mesma', 'mesmos', 'mesmas',
+    'assim', 'aqui', 'ali', 'lá', 'bem', 'mal', 'já', 'ainda',
+    'tem', 'têm', 'tinha', 'tiveram', 'tivemos',
+    'depois', 'antes', 'entretanto', 'também', 'então', 'tambem',
+    'cada', 'qual', 'quem', 'cujo', 'cuja', 'cujos', 'cujas',
+    'onde', 'quando', 'seja', 'for', 'estava',
+    'nesse', 'neste', 'naquele', 'deste', 'dessa', 'disso', 'nisso', 'nele', 'nela', 'neles', 'nelas'
+)
+AND LENGTH(palavra) > 2
 GROUP BY palavra
 ORDER BY ocorrencias DESC
